2a91e26d85bec8dcd8e9e6a7c3cf2cae8ea1e826
Thor Kell <thor@tide-pool.ca>
2022-01-13 20:02:35 -0500

    justly tuned chords

--- morning_sound_bath.py
+++ morning_sound_bath.py
@@ -17,15 +17,15 @@ def elapsed_time_to_velocity(current_time, start_time, total_duration_minutes):
     new_range = math.pi
     zero_to_pi = (((current_minutes - 0) * new_range) / old_range) + 0
     sine_value = math.sin(zero_to_pi)
-    return sine_value / 10
+    return sine_value / 20
 
 
-def get_notes(octaves, pitches):
-    notes = []
-    for pitch in pitches:
-        note = (random.choice(octaves) * 12) + pitch
-        notes.append(note)
-    return notes
+def get_frequencies(octaves, frequencies):
+    final_hz = []
+    for frequency in frequencies:
+        hz = frequency * (random.choice(octaves))
+        final_hz.append(hz)
+    return final_hz
 
 
 def get_durations(num_attacks, total_duration):
@@ -44,8 +44,8 @@ def get_start_times(durations):
     return starts_and_durations
 
 
-def add_notes(starts_and_durations, note):
-    return [(s_d[0], s_d[1], note) for s_d in starts_and_durations]
+def add_notes(starts_and_durations, hz):
+    return [(s_d[0], s_d[1], hz) for s_d in starts_and_durations]
 
 
 def play_note(osc_id, num_oscs, num_speakers, note, velocity, duration):
@@ -53,10 +53,6 @@ def play_note(osc_id, num_oscs, num_speakers, note, velocity, duration):
     second_breakpoint_ms = round((duration) * 1000)
     breakpoint_string = f"{first_breakpoint_ms},10,{second_breakpoint_ms},0.05,500,0"
 
-    if note > 72:
-        velocity = velocity / 1.5
-    elif note > 60:
-        velocity = velocity / 1.25
     print("sending ... ", osc_id, note, velocity, duration, breakpoint_string)
     alles.send(
         osc=osc_id,
@@ -64,7 +60,7 @@ def play_note(osc_id, num_oscs, num_speakers, note, velocity, duration):
         bp0_target=alles.TARGET_AMP,
         wave=alles.TRIANGLE,
         vel=velocity,
-        note=note,
+        freq=note,
         client=osc_id % num_speakers,
     )
     osc_id += 1
@@ -72,15 +68,29 @@ def play_note(osc_id, num_oscs, num_speakers, note, velocity, duration):
     return next_osc_id
 
 
-chords_of_the_week = {
-    0: [5, 9, 0],  # f+
-    1: [0, 4, 7],  # c+
-    2: [7, 11, 2],  # g+
-    3: [2, 6, 9],  # d+
-    4: [9, 1, 4],  # a+
-    5: [4, 8, 11],  # e+
-    6: [11, 3, 6],  # b+
-}
+def make_just_intonation_chords(starting_hz):
+    frequencies = {}
+    octave = starting_hz * 2
+    for i in range(0, 7):
+        if i == 0:
+            root = starting_hz
+        else:
+            root = starting_hz * ((3 / 2) ** i)
+
+        while root > octave:
+            root = root / 2
+
+        third = root * (5 / 4)
+        while third > octave:
+            third = third / 2
+
+        fifth = root * (3 / 2)
+        while third > octave:
+            fifth = fifth / 2
+
+        frequencies[i] = [root, third, fifth]
+    return frequencies
+
 
 # python morning_sound_bath --start_time 0700 --duration_in_minutes 90
 if __name__ == "__main__":
@@ -93,7 +103,8 @@ if __name__ == "__main__":
     end_time = start_time + dt.timedelta(minutes=args.duration_in_minutes)
     total_duration_minutes = args.duration_in_minutes
 
-    octaves = [4, 5, 6]
+    c = 256
+    hz_octaves = [1, 2, 4]
     num_oscs = 6
     num_speakers = 3
     time_to_sleep = 300
@@ -109,20 +120,19 @@ if __name__ == "__main__":
             continue
         alles.reset()  # just in case, before we start ..
 
-        pitches = chords_of_the_week[weekday]
+        frequencies = make_just_intonation_chords(c)
+        hz_to_play = get_frequencies(hz_octaves, frequencies[weekday])
         velocity = elapsed_time_to_velocity(
             now, start_time_today, total_duration_minutes
         )
-
         next_voice = random.randint(60, 240)
 
         all_events = []
-        notes = get_notes(octaves, pitches)
-        for note in notes:
+        for hz in hz_to_play:
             num_attacks = random.randint(3, 10)
             durations = get_durations(num_attacks, next_voice)
             starts_and_durations = get_start_times(durations)
-            times_and_note = add_notes(starts_and_durations, note)
+            times_and_note = add_notes(starts_and_durations, hz)
             all_events.extend(times_and_note)
         # sort by start time
         sorted_events = sorted(all_events, key=lambda event: event[0])
