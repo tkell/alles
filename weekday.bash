1bcb90db58735dc8ae96d5273252cf8f0179f472
Thor Kell <thor@tide-pool.ca>
2022-01-18 20:47:34 -0500

    change weekday script

--- weekday.bash
+++ weekday.bash
@@ -1,3 +1,2 @@
-# Run me on Sunday nights!
-python morning_sound_bath.py --start_time 0645 --duration_in_minutes 75 &
+python morning_sound_bath.py --duration_in_minutes 75 &
 caffeinate -w $(ps aux | grep "[m]orning_sound_bath" | awk '{print $2}')
